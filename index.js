deeaf3cd2717a39973da112c48419bbd6be50a5c
Omar <omanpios88@gmail.com>
2023-11-27 18:19:35 -0500

    Start coding index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const BASE_URL = "https://api.jikan.moe/v4/anime";
+
+app.get("/", (req, res) => {
+  try {
+    const respose = axios.get(BASE_URL);
+  } catch (error) {}
+});
+
+app.listen(port, () => {
+  `Server running on port: ${port}`;
+});
