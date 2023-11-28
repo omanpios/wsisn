858c0d146f8f5d173d64ee8557d98f49188483b9
Omar <omanpios88@gmail.com>
2023-11-27 19:09:22 -0500

    Modify hardcoded query params to return data

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ const BASE_URL = "https://api.jikan.moe/v4/anime";
 
 app.get("/", async (req, res) => {
   const sfw = false;
-  const type = "movie";
-  const min_score = 8;
+  const type = "tv";
+  const min_score = 7;
   const status = "complete";
-  const rating = "rx";
+  const rating = "r17";
 
   try {
     const response = await axios.get(
       `${BASE_URL}?sfw=${sfw}&type=${type}&min_score=${min_score}&status=${status}&rating=${rating}`
     );
-    const result = response.data.data;
+    const result = response.data.data[1];
     res.render("index.ejs", {
-      title: result[0].title,
-      title_japanese: result[0].title_japanese,
-      synopsis: result[0].synopsis,
-      image_url: result[0].images.jpg.image_url,
+      title: result.title,
+      title_japanese: result.title_japanese,
+      synopsis: result.synopsis,
+      image_url: result.images.jpg.image_url,
     });
   } catch (error) {
     console.log(error);
