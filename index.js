78ce50db50102e154105eafec5079466d6581c94
Omar <omanpios88@gmail.com>
2023-11-28 08:36:17 -0500

    -Add filters to index.ejs -Add dynamic query params taking values from index

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-const BASE_URL = "https://api.jikan.moe/v4/anime";
+const BASE_URL = "https://api.jikan.moe/v4";
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  const sfw = false;
-  const type = "tv";
-  const min_score = 7;
-  const status = "complete";
-  const rating = "r17";
-
   try {
-    const response = await axios.get(
-      `${BASE_URL}?sfw=${sfw}&type=${type}&min_score=${min_score}&status=${status}&rating=${rating}`
-    );
-    const result = response.data.data[1];
+    const response = await axios.get(`${BASE_URL}/random/anime`);
+    const result = response.data.data;
     res.render("index.ejs", {
       title: result.title,
       title_japanese: result.title_japanese,
@@ -29,6 +24,39 @@ app.get("/", async (req, res) => {
   }
 });
 
+app.post("/", async (req, res) => {
+  const request = req.body;
+  console.log(request);
+  const sfw = request.sfw;
+  const type = request.type;
+  const min_score = Number(request.score);
+  const status = request.status;
+  const rating = request.rating;
+  console.log(`Score: ${min_score}`);
+
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/anime?sfw=${sfw}&type=${type}&status=${status}&rating=${rating}&min_score=${min_score}`
+    );
+    console.log();
+    const randomIndex = Math.floor(Math.random() * response.data.data.length);
+    const result = response.data.data[randomIndex];
+    res.render("index.ejs", {
+      title: result.title,
+      title_japanese: result.title_japanese,
+      synopsis: result.synopsis,
+      image_url: result.images.jpg.image_url,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("index.ejs", {
+      title: "result.title",
+      title_japanese: "result.title_japanese",
+      synopsis: "result.synopsis",
+      image_url: "result.images.jpg.image_url",
+    });
+  }
+});
 app.listen(port, () => {
   console.log(`Server running on port: ${port} - ${new Date()}`);
 });
