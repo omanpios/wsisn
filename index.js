5a0a1423ae3cf3b5dbd2e64c4fba24e507077efa
Omar <omanpios88@gmail.com>
2023-11-27 19:01:46 -0500

    Call jikan API with hardcoded query params to check I am mapping data properly

--- index.js
+++ index.js
@@ -5,12 +5,30 @@ const app = express();
 const port = 3000;
 const BASE_URL = "https://api.jikan.moe/v4/anime";
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const sfw = false;
+  const type = "movie";
+  const min_score = 8;
+  const status = "complete";
+  const rating = "rx";
+
   try {
-    const respose = axios.get(BASE_URL);
-  } catch (error) {}
+    const response = await axios.get(
+      `${BASE_URL}?sfw=${sfw}&type=${type}&min_score=${min_score}&status=${status}&rating=${rating}`
+    );
+    const result = response.data.data;
+    res.render("index.ejs", {
+      title: result[0].title,
+      title_japanese: result[0].title_japanese,
+      synopsis: result[0].synopsis,
+      image_url: result[0].images.jpg.image_url,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
 });
 
 app.listen(port, () => {
-  `Server running on port: ${port}`;
+  console.log(`Server running on port: ${port} - ${new Date()}`);
 });
